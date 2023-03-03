2ac0332578d94d2c43aa75f834e9b96ab71fe067
bright2013 <devkimmi@gmail.com>
2023-03-03 23:48:29 +0900

    Add problem 237

---
+++ Algorithm/java/leetcode/Problem237.java
@@ -0,0 +1,21 @@
+public class Problem237 {
+    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+    public void deleteNode(ListNode node) {
+
+        while (node.next.next != null) {
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+
+    }
+}
